3c2765c5a4a96ee3d531378364f52b61beb1649b
Duleesha Gamalathge <duleeshagamalathge@gmail.com>
2024-01-01 03:38:05 +0530

    implement CRUD operations for blog posts

---
+++ API/Blog/Blog/Data/BlogDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDBContext : DbContext
+    {
+        public BlogDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //DBSet
+        public DbSet<Post> Posts { get; set; }
+    }
+}
